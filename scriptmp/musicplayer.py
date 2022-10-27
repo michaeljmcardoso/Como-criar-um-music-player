fa3ea1d9e1a2c9d529225e5b7c211625f6e5e5ec
Michael <91438312+michaeljmcardoso@users.noreply.github.com>
2022-10-27 14:25:25 -0300

    Update musicplayer.py

--- scriptmp/musicplayer.py
+++ scriptmp/musicplayer.py
@@ -1,7 +1,8 @@
+# importando o pacote necessário pygame
 import pygame
 
 pygame.init()
-pygame.mixer.music.load('suamusica.ogg')
+pygame.mixer.music.load('suamusica.ogg') # local da sua música no formato .ogg
 pygame.mixer.music.play()
 pygame.event.wait()
 input()
