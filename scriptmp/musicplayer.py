41d5565d11d3bcea2293e243515adb30242e1220
Michael <91438312+michaeljmcardoso@users.noreply.github.com>
2023-01-10 10:26:55 -0300

    Update musicplayer.py
    Atualização do código para remoção das linhas pygame.event.wait() e  input. Inclusão de um Loop (while). E melhor documentação do projeto.

--- scriptmp/musicplayer.py
+++ scriptmp/musicplayer.py
@@ -1,8 +1,26 @@
-# importando o pacote necessário pygame
+#instalando módulo pygame
+%pip install pygame # no vs code
+!pip install pygame # no pycharm ou colab
+
+# Importando a biblioteca necessária pygame
 import pygame
 
-pygame.init()
-pygame.mixer.music.load('suamusica.ogg') # local da sua música no formato .ogg
+# Para inicializar todos os módulos Pygame importados
+# pygame.init()
+
+# Para inicializar apenas o mixer do PyGame
+pygame.mixer.init()
+
+# Carregar um arquivo de música para reprodução
+pygame.mixer.music.load('sua_musica.mp3') # local da sua música
+
+# Iniciar a reprodução do fluxo de música
 pygame.mixer.music.play()
-pygame.event.wait()
-input()
+
+# Esse loop verifica se o canal de música está em uso,
+# quando não mais em uso, ele sai do loop
+while pygame.mixer.music.get_busy():
+    continue # usado para voltar ao início do loop
+
+# sair do programa
+pygame.quit()
